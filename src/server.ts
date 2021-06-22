18e5d2003e11f95c2f6bcbe6335ed3ba44e09be6
Adam-Almeida <adamportifolio@gmail.com>
2021-06-21 23:30:40 -0300

    Create DB and Migration User

--- src/server.ts
+++ src/server.ts
@@ -3,22 +3,7 @@ import express, { response } from "express";
 //@types/express Biblioteca de Tipos
 const app = express();
 
-/**
- * MÉTODOS HTTP
- * GET    -> Utilizado para buscar uma informação
- * POST   -> Utilizado pra criar/salvar uma informação
- * DELETE -> Remover uma informação
- * PUT    -> Atualizar uma informação
- * PATCH  -> Atualizar um dado especifico
- */
+import "./database";
 
-app.get("/test", (request, response)=>{
-    return response.send("Hello Wolrd");
-});
 
-app.post("/test-post", (request, response)=>{
-    return response.send("Hello World pelo método post");
-});
-
-//http://localhost:3000
 app.listen(3000, ()=> {console.log("Server is Running");});
